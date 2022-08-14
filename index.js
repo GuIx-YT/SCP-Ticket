45c034e52156e48e4651a6fd8b08572221a646b2
GuIx-YT <86738649+GuIx-YT@users.noreply.github.com>
2022-08-14 15:16:26 +0200

    init commit 1.0.3

--- index.js
+++ index.js
@@ -25,20 +25,20 @@ Client.on("ready", () =>{
         Client.user.setActivity(status[rstatus], {type: "WATCHING"});
     }; setImmediate(randomStatus, 2000)
 
-    // var row = new Discord.MessageActionRow()
-    //     .addComponents(new Discord.MessageButton()
-    //         .setCustomId("open-ticket")
-    //         .setLabel("Ouvrir un Ticket")
-    //         .setStyle("PRIMARY")
-    //     );
+    var row = new Discord.MessageActionRow()
+        .addComponents(new Discord.MessageButton()
+            .setCustomId("open-ticket")
+            .setLabel("Ouvrir un Ticket")
+            .setStyle("PRIMARY")
+        );
 
-    // const embed = new Discord.MessageEmbed()
-    //     .setAuthor("SCP Breach RP")
-    //     .setColor("GREY")
-    //     .setTitle("**Ticket**")
-    //     .setDescription("Vous pouvez faire un Ticket en appuyant sur le bouton ci-dessous")
+    const embed = new Discord.MessageEmbed()
+        .setAuthor("SCP Breach RP")
+        .setColor("GREY")
+        .setTitle("**Ticket**")
+        .setDescription("Vous pouvez faire un Ticket en appuyent sur le bouton ci-dessous")
     
-    // Client.channels.cache.get("991810648881254481").send({embeds: [embed], components: [row]});
+    Client.channels.cache.get("991810648881254481").send({embeds: [embed], components: [row]});
 
 });
 
@@ -74,6 +74,8 @@ Client.on("interactionCreate", interaction => {
                     .setDescription("Voici votre Ticket <@" + interaction.user.id + "> Vous-êtes en contacte avec les membres du Staff")
                 
                 channel.send({embeds: [embed], components: [row]});
+
+                interaction.reply({content: "Ticket correctement créé", ephemeral: true});
             });
         }
         else if(interaction.customId === "close-ticket"){
